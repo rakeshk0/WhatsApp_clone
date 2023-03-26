6fd5056ea1c1e5d02b86cdfed81924f47484706d
Rakesh <rakeskumarkumar0@gmail.com>
2023-03-26 22:54:30 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -0,0 +1,31 @@
+package com.example.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.whatsapp.databinding.ActivitySignUpBinding;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+        getSupportActionBar().hide();
+
+
+
+    }
+}
